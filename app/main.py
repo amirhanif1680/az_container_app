b12928c566815b873e92e60e19d1c472e659ee3c
amhanif <amirhanif3268@gmail.com>
2024-07-14 15:18:19 +0500

    K1

--- app/main.py
+++ app/main.py
@@ -7,3 +7,6 @@ app = FastAPI()
 def read_root():
     return {"Hello": "Rooman", "World2": "Ayan Amir"}
 
+@app.get("/developer/")
+def developer_details():
+    return {"Name": "Amir Hanif", "Institute": "PIAIC"}
